c27fb5591ffac653ca6f968152d668f1e4b5a5a0
samson <samsonlabz@gmail.com>
2024-04-04 19:11:33 +0200

    Update FPSControllerSettingsEditor.cs

--- Assets/Demo/Scripts/Editor/FPSControllerSettingsEditor.cs
+++ Assets/Demo/Scripts/Editor/FPSControllerSettingsEditor.cs
@@ -9,17 +9,18 @@ namespace Demo.Scripts.Editor
     [CustomEditor(typeof(FPSControllerSettings), true)]
     public class FPSControllerSettingsEditor : UnityEditor.Editor
     {
-        private TabInspectorWidget _tabInspectorWidget;
+        //private TabInspectorWidget _tabInspectorWidget;
         
         private void OnEnable()
         {
-            _tabInspectorWidget = new TabInspectorWidget(serializedObject);
-            _tabInspectorWidget.Init();
+            //_tabInspectorWidget = new TabInspectorWidget(serializedObject);
+            //_tabInspectorWidget.Init();
         }
         
         public override void OnInspectorGUI()
         {
-            _tabInspectorWidget.Render();
+            base.OnInspectorGUI();
+            //_tabInspectorWidget.Render();
         }
     }
 }
